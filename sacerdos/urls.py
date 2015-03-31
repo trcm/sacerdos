13e95e906181ac854237aa45ff757f7838146125
Tom <midson.trc@gmail.com>
2015-03-31 14:15:04 +1000

    Added all the bower and npm files for the project.  Basic wallfly app is now scaffolded

--- sacerdos/urls.py
+++ sacerdos/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sacerdos.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'sacerdos.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^', include('wallfly.urls')),
 )
--- wallfly/views.py
+++ wallfly/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import status
+
+from wallfly.models import *
+from wallfly.serializers import PropertySerializer
+
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+from django.db.utils import IntegrityError
+from django.core import serializers
+
+
+def home(request):
+    return render(request, 'base.djhtml')
+
+
