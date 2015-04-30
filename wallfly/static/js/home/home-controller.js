ea28fb095e8bb3854d9cffde0c8f53b3cc9f4475
Tom <midson.trc@gmail.com>
2015-04-30 10:40:59 +1000

    first steps for css done

--- wallfly/static/js/home/home-controller.js
+++ wallfly/static/js/home/home-controller.js
@@ -2,7 +2,7 @@ angular.module('wallfly')
   .controller('HomeController', ['$scope', '$http', '$window', 'resolveProperties', 'Property',function($scope, $http, $window, resolveProperties, Property) {
     // use the ReolveProperties method in the application
     $scope.properties = resolveProperties.data;
-
+    
     if ($scope.properties.prop) {
       $scope.prop = $scope.properties.prop;
     }
