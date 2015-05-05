3c515e8ec0d8de3bb6e7e4bd39ee9fedd3191870
Isaac <isaachill13@gmail.com>
2015-05-05 13:07:46 +1000

    added legit looking data

--- wallfly/static/js/property/property-controller.js
+++ wallfly/static/js/property/property-controller.js
@@ -11,7 +11,7 @@ angular.module('wallfly')
 
     $scope.currIssues = lodash.filter($scope.issues, {"resolved" : 0});
     console.log($scope.currIssues.length);
-    
+
     //sets the ui for the calendar app
     $scope.uiConfig = {
       calendar:{
@@ -37,12 +37,12 @@ angular.module('wallfly')
 
     /* event source that contains custom events on the scope */
     $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+      {title: 'Wall Repair',start: new Date(y, m, d + 1, 12, 0),allDay: false},
+      {title: 'Wall Repair',start: new Date(y, m, d + 6, 9, 0),allDay: false},
+      {title: 'Wall Repair',start: new Date(y, m, d + 4, 11, 0),allDay: false},
+      {title: 'Wall Repair',start: new Date(y, m, d + 12, 10, 0),allDay: false}
+
+
     ];
 
     $scope.eventSources = [$scope.events];
